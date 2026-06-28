9397f4b76e4e29700d1180c98e9cf485b7e04134
Sara Deshpande <saradeshpande1807@gmail.com>
2026-06-27 22:47:37 -0400

    add base CSS styles and Navbar styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
-  const title= "GitScan";
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Home></Home>
       <div className="content">
-        <h1>
-          {title}
-        </h1>
+      <Home></Home>
       </div>
     </div>
   );
