38472aad61c6cd404320bae1fba802a14fe84122
Sara Deshpande <saradeshpande1807@gmail.com>
2026-06-27 21:55:49 -0400

    add Navbar and Home Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   const title= "GitScan";
   return (
     <div className="App">
+      <Navbar></Navbar>
+      <Home></Home>
       <div className="content">
         <h1>
           {title}
