15c5f54645c5070412bd29af9c1b714481e79805
Sara Deshpande <saradeshpande1807@gmail.com>
2026-06-27 21:55:49 -0400

    add Navbar and Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Analyze your Github profile</h2>
+            <p>Get recruiter perspective feedback based on your target roles</p>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>GitScan</h1>
+            <div className="links">
+                <a href="/"></a>
+                <a href="/create">Home page</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
