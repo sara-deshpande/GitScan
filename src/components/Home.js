1716489540b08441b368cb1663f2ea313f947b07
Sara Deshpande <saradeshpande1807@gmail.com>
2026-06-28 22:32:23 -0400

    Add Button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import './Home.css';
 
 const Home = () => {
+    const handleClick = () => {
+        console.log('Profile Scanned');
+    }
+
     return ( 
         <div className="home">
             <h2>Analyze your Github profile</h2>
             <p>Get recruiter perspective feedback based on your target roles</p>
+            <button onClick={handleClick}> Scan Profile </button>
         </div>
      );
 }
  
-export default Home;
+export default Home; 
