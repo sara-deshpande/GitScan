cc2b2a13965eaf81f1bb1b8beeae1a5619cb8cdd
Sara Deshpande <saradeshpande1807@gmail.com>
2026-09-11 21:30:49 -0400

    SEtup React Router with four page routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
+import ScorePage from './components/ScorePage';
+import ActionPage from './components/ActionPage';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-      <Home></Home>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/results" element={<ScorePage />} />
+            <Route path="/action-plan" element={<ActionPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
