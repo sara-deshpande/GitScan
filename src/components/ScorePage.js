272752e34ae0d5005b10efd24ad0aab8e3a3bab3
Sara Deshpande <saradeshpande1807@gmail.com>
2026-09-11 21:19:10 -0400

    Create 3 new pages to help setup Router

---
+++ src/components/ScorePage.js
@@ -0,0 +1,12 @@
+import './ScorePage.css';
+
+const ScorePage = () => {
+    return (  
+        <div className="score-page">
+            <h2>AI Analysis</h2>
+            <p>AI scores and feedback will show here</p>
+        </div>
+    );
+}
+ 
+export default ScorePage;
