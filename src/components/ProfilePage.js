d06063e41e1fe63f70a3690c11b970f0da088cfc
Sara Deshpande <saradeshpande1807@gmail.com>
2026-09-16 23:34:43 -0400

    add route parameters for username in profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,9 +1,12 @@
 import './ProfilePage.css';
+import {useParams} from "react-router-dom";
 
 const ProfilePage = () => {
+
+    const { username }= useParams();
     return (
         <div className="profilepage">
-            <h2>Profile Overview</h2>
+            <h2>Profile Overview - {username} </h2>
             <p>GitHub profile data will show here</p>
         </div>
     );
